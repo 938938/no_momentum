8915c72d7044045bc17f083d7a9f3932b6ee093c
938938 <938kjh@gmail.com>
2022-02-20 22:18:01 +0900

    commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert("hi");
+// alert("hi");
+
+function change(){
+  const but = document.getElementById("btn");
+  but.style.color = "red";
+  but.innerText = "얍??";
+  const bod = document.getElementById("body");
+  bod.style.backgroundColor = "blue";
+}
+
+document.getElementById("btn").addEventListener("click",change);
