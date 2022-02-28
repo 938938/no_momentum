a88e7e45255534e76e75b119001c897ce54ee392
938938 <938kjh@gmail.com>
2022-02-28 23:04:32 +0900

    commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // alert("hi");
 
-function change(){
-  const but = document.getElementById("btn");
-  but.style.color = "red";
-  but.innerText = "얍??";
-  const bod = document.getElementById("body");
-  bod.style.backgroundColor = "blue";
-}
+// function change(){
+//   const but = document.getElementById("btn");
+//   but.style.color = "red";
+//   but.innerText = "얍??";
+//   const bod = document.getElementById("body");
+//   bod.style.backgroundColor = "orange";
+// }
 
-document.getElementById("btn").addEventListener("click",change);
+// document.getElementById("btn").addEventListener("click",change);
+
+let a = 5;
+const b = 2;
+const myName = "JH";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("Hello "+myName);
