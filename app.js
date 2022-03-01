c0a45eec8ee061fbde5bc09a50b0e7ded6c566bf
938938 <938kjh@gmail.com>
2022-03-02 01:01:37 +0900

    #2.9

--- app.js
+++ app.js
@@ -36,12 +36,37 @@ const player = {
   name:"jh",
   points:1,
   fat:false,
+  sayHello: function(){
+    console.log("hello!");
+  },
 };
 
-console.log(player);
 console.log(player.name);
-console.log(player["name"]);
+player.sayHello();
 
-player.points=player.points+2;
-player.gender="female";
-console.log(player);
+// console.log(player);
+// console.log(player.name);
+// console.log(player["name"]);
+
+// player.points=player.points+2;
+// player.gender="female";
+// console.log(player);
+
+// function sayHello(name, age){
+//   console.log("Hello! my name is "+name, "It is "+age );
+// };
+
+// sayHello("nico", 10);
+// sayHello("dal", 23);
+// sayHello("lyn", 5);
+
+
+function plus(a,b){
+  console.log(a+b);
+}
+function divide(a,b){
+  console.log(a/b);
+}
+
+plus(1,2);
+divide(2,1);
