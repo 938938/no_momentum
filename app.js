0d951b8a6ba655b65af572ab627e9bcc34d35bcd
938938 <938kjh@gmail.com>
2022-03-03 23:54:08 +0900

    2.11

--- app.js
+++ app.js
@@ -1,72 +1,35 @@
-// alert("hi");
-
-// function change(){
-//   const but = document.getElementById("btn");
-//   but.style.color = "red";
-//   but.innerText = "얍??";
-//   const bod = document.getElementById("body");
-//   bod.style.backgroundColor = "orange";
-// }
-
-// document.getElementById("btn").addEventListener("click",change);
-
-const a = 5;
-const b = 2;
-let myName = "JH";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("Hello "+myName);
-
-myName = "jihyeon";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("Hello "+myName);
-
-let week = ["mon","tue","wed","thu","fri","sat","sun"];
-console.log(week[4]);
-
-week.push("weekend");
-console.log(week);
-
-const player = {
-  name:"jh",
-  points:1,
-  fat:false,
-  sayHello: function(){
-    console.log("hello!");
+const calculator = {
+  plus: function (a,b){
+    return a + b;
+  },
+  minus: function (a,b){
+    return a - b;
+  },
+  times: function (a,b){
+    return a * b;
+  },
+  divide: function (a,b){
+    return a / b;
+  },
+  power: function (a,b){
+    return a ** b;
   },
 };
 
-console.log(player.name);
-player.sayHello();
-
-// console.log(player);
-// console.log(player.name);
-// console.log(player["name"]);
-
-// player.points=player.points+2;
-// player.gender="female";
-// console.log(player);
-
-// function sayHello(name, age){
-//   console.log("Hello! my name is "+name, "It is "+age );
-// };
-
-// sayHello("nico", 10);
-// sayHello("dal", 23);
-// sayHello("lyn", 5);
+const plusResult = calculator.plus(2,1);
+const minusResult = calculator.minus(plusResult,10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
+// const.log(plusResult);
 
 
-function plus(a,b){
-  console.log(a+b);
-}
-function divide(a,b){
-  console.log(a/b);
-}
+// const age = 20;
 
-plus(1,2);
-divide(2,1);
+// function calculatorKrAge(ageOfForeigner){
+//   return ageOfForeigner + 2;
+// }
+
+// const KrAge = calculatorKrAge(age);
+
+// console.log(KrAge);
