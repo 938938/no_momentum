c356e9d73721fb2c4e3e9219048d919a60037390
938938 <938kjh@gmail.com>
2022-03-06 23:49:21 +0900

    #4.4

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASS = "hidden";
 
 function onLogin(event){
   event.preventDefault();
-  console.log(event);
-  // const userName = loginInput.value;
+  const userName = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASS);
+  console.log(userName);
+  greeting.innerText = `Hello! ${userName}`;
+  greeting.classList.remove(HIDDEN_CLASS);
 }
 
 loginForm.addEventListener("submit",onLogin);
